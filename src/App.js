9d9d31b10961b34d0a13bd80e3be369bac7e5c2f
LeGall, Kevin <kevin.legall@consumer.org>
2021-08-02 09:29:47 -0400

    august 2.3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.scss';
+import Socials from './components/Socials';
+import React from 'react';
 
 function App() {
   return (
@@ -19,30 +21,30 @@ function App() {
         </ul>
       </nav>
       </header>
-      <div class="parallax first">
-        <div class="text">
+      <div className="parallax first">
+        <div className="text">
           <h1>Space Bacon</h1>
         </div>
       </div>
-      <div class="container" id="about">
+      <div className="container" id="about">
         <h2>About Us</h2>
         <p>The origin of Space Bacon traces back to the depths of Fordham University basement parties on the gritty streets of the Bronx. Since then, Space Bacon has grown from frat fests to the main stage, quickly conquering the title as one of the brightest up-and-coming jam bands in the region.</p>
         <p>Space Bacon has performed at The Capitol Theater, Irving Plaza, Brooklyn Bowl, TLA, Bowery Ballroom, as well as toured all around the country, performing with artists such as Umphrey’s McGee, Tauk, Spafford, Pigeons Playing Ping Pong, Papadosio, Breaking Biscuits, Consider the Source, and many more!</p>
         <br /><br />
-        <div class="images">
-          <div class="img-wrap">
+        <div className="images">
+          <div className="img-wrap">
             <img src="images/band/chris.jpeg" alt="chris" />
             <h4>Chris Gironda - Keyboards</h4>
           </div>
-          <div class="img-wrap">
+          <div className="img-wrap">
             <img src="images/band/jack.jpeg" alt="jack" />
             <h4>Jack Willard - Guitar</h4>
           </div>
-          <div class="img-wrap">
+          <div className="img-wrap">
             <img src="images/band/sam.jpeg" alt="sam" />
             <h4>Sam Crespo - Drums</h4>
           </div>            
-          <div class="img-wrap">
+          <div className="img-wrap">
             <img src="images/band/kevin.jpeg" alt="kevin" />
             <h4>Kevin LeGall - Bass</h4>
           </div>            
@@ -50,10 +52,10 @@ function App() {
         <hr />
         <Socials />
       </div>
-      <div class="parallax second"></div>
-      <div class="container" id="tour">
+      <div className="parallax second"></div>
+      <div className="container" id="tour">
         <h2>Tour</h2>
-        <div class="bit-widget-initializer"
+        <div className="bit-widget-initializer"
           data-artist-name="Space Bacon"
           data-text-color="#cccccc"
           data-link-color="#cccccc"
@@ -63,9 +65,9 @@ function App() {
           data-display-lineup="false"
           data-separator-color="rgba(124,124,124,0.25)"></div>
       </div>
-      <div class="parallax third"></div>
-      <div class="container album" id="album">
-        <div class="row">
+      <div className="parallax third"></div>
+      <div className="container album" id="album">
+        <div className="row">
           <div>
             <h2>Cone by Space Bacon</h2>
             <h3>The Debut Full Length Studio Album</h3>
@@ -80,24 +82,26 @@ function App() {
           </div>
         </div>
       </div>
-      <div class="parallax fourth"></div>
-      <div class="container no-mobile">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/0KbIdC6dKA0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div className="parallax fourth"></div>
+      <div className="container no-mobile">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0KbIdC6dKA0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
-      <div class="parallax fifth no-mobile"></div>
-      <div class="container" id="contact">
+      <div className="parallax fifth no-mobile"></div>
+      <div className="container" id="contact">
         <h2>Contact Us</h2>
         <h3>For Booking Please Contact:</h3>
         <p>Pivotal Agency // Sam Hutchinson - Booking Agent</p>
-        <p>SpaceBaconBooking@gmail.com</p>
-        <br />
+        <a href="mailto:SpaceBaconBooking@gmail.com">SpaceBaconBooking@gmail.com</a>
+        <br/><br/>
+        <hr />
+        <br/>
         <h3>For Press Inquiries and General Contact:</h3>
         <p>Harrison Waxenberg - Manager</p>
-        <p>SpaceBaconMGMT@gmail.com</p>       
+        <a href="mailto:SpaceBaconMGMT@gmail.com">SpaceBaconMGMT@gmail.com</a>     
       </div>
       <footer>
         <Socials />
-        <div class="legal">
+        <div className="legal">
         © Space Bacon, LLC | Website Designed & Developed by <a href="mailto: Kev1nL3Gall@gmail.com">Kevin LeGall</a>
         <hr />
         Photos By Bill Murray
